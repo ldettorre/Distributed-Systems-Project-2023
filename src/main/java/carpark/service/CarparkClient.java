592df40d5ec5eb1ee15436b9b52829a31bbcea53
Luca D'Ettorre <ldettorre21@gmail.com>
2023-04-09 18:05:46 +0100

    Created a client file to start with the AccessCarpark Unary method. A request is built using the idNumber variable, then passed through the blocking stub and assigned to the response. All within a try/catch/finally. Goal will be to create a server file then migrate the client code to a gui file when working

---
+++ src/main/java/carpark/service/CarparkClient.java
@@ -0,0 +1,37 @@
+package carpark.service;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import carpark.service.carparkServicesGrpc.carparkServicesBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+public class CarparkClient {
+	private static final Logger logger = Logger.getLogger(CarparkClient.class.getName());
+	public static void main(String[] args) throws Exception {
+		String host = "localhost";
+		int port = 50051;
+		
+		ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+		
+		carparkServicesBlockingStub blockingStub = carparkServicesGrpc.newBlockingStub(channel);
+		CarparkClient client = new CarparkClient();
+		
+		
+		try {
+			String idNumber = "2";
+			AccessRequest request = AccessRequest.newBuilder().setIdNumber(idNumber).build();
+			AccessResponse response = blockingStub.accessCarpark(request);
+			logger.info("Client Side Started..");
+		}
+		catch(StatusRuntimeException e) {
+			logger.log(Level.WARNING, "RCP failed: (0)", e.getStatus());
+		}
+		finally {
+			channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+		}
+	}
+}
